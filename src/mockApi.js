553a59586a0a8d6b5ee2c36a25edc27a718602a3
banu-101291 <sifnaz78@gmail.com>
2023-10-13 22:07:14 +0530

    final

--- src/mockApi.js
+++ src/mockApi.js
@@ -1,52 +1,51 @@
 const books = [
-    { id: 1, title: 'Book 1', author: 'Author 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2' },
-    // Add more mock data as needed
-  ];
-  
-  export const getAllBooks = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(books);
-      }, 500);
-    });
-  };
-  
-  export const createBook = (book) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const newBook = { ...book, id: books.length + 1 };
-        books.push(newBook);
-        resolve(newBook);
-      }, 500);
-    });
-  };
-  
-  export const updateBook = (book) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const index = books.findIndex((b) => b.id === book.id);
-        if (index !== -1) {
-          books[index] = book;
-          resolve(book);
-        } else {
-          resolve(null);
-        }
-      }, 500);
-    });
-  };
-  
-  export const deleteBook = (id) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const index = books.findIndex((b) => b.id === id);
-        if (index !== -1) {
-          const deletedBook = books.splice(index, 1)[0];
-          resolve(deletedBook);
-        } else {
-          resolve(null);
-        }
-      }, 500);
-    });
-  };
-  
+  { id: 1, title: 'Book 1', author: 'Author 1' },
+  { id: 2, title: 'Book 2', author: 'Author 2' },
+  // Add more mock data as needed
+];
+
+export const getAllBooks = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(books);
+    }, 500);
+  });
+};
+
+export const createBook = (book) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const newBook = { ...book, id: books.length + 1 };
+      books.push(newBook);
+      resolve(newBook);
+    }, 500);
+  });
+};
+
+export const updateBook = (book) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const index = books.findIndex((b) => b.id === book.id);
+      if (index !== -1) {
+        books[index] = book;
+        resolve(book);
+      } else {
+        resolve(null);
+      }
+    }, 500);
+  });
+};
+
+export const deleteBook = (id) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const index = books.findIndex((b) => b.id === id);
+      if (index !== -1) {
+        const deletedBook = books.splice(index, 1)[0];
+        resolve(deletedBook);
+      } else {
+        resolve(null);
+      }
+    }, 500);
+  });
+};
